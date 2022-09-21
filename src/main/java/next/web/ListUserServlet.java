806cee73b613e5f88bb0fa7149f375ce50f6852e
nathan29849 <phs960418@gmail.com>
2022-09-21 16:26:49 +0900

    feat: [#19] ListUserServlet - 로그인 하지 않은 유저 볼 수 없도록 설정

--- src/main/java/next/web/ListUserServlet.java
+++ src/main/java/next/web/ListUserServlet.java
@@ -2,6 +2,7 @@ package next.web;
 
 import core.db.DataBase;
 import java.io.IOException;
+import java.util.Arrays;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,6 +16,15 @@ public class ListUserServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (req.getCookies() == null){
+            throw new IllegalStateException("로그인되지 않은 유저입니다.");
+        }
+
+        Arrays.stream(req.getCookies())
+            .filter(c -> c.getName().equals("login"))
+            .findAny()
+            .orElseThrow(() -> new IllegalStateException("로그인되지 않은 유저입니다."));
+
         req.setAttribute("users", DataBase.findAll());
         RequestDispatcher rd = req.getRequestDispatcher("/user/list.jsp");
         rd.forward(req, resp);
