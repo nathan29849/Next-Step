0972dccc4bcfc5aba8b7439c5df0bc38a0888439
nathan29849 <phs960418@gmail.com>
2022-09-21 15:17:07 +0900

    feat: [#19] LoginServlet - Cookie 추가

--- src/main/java/next/web/LoginServlet.java
+++ src/main/java/next/web/LoginServlet.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,6 +17,8 @@ import org.slf4j.LoggerFactory;
 public class LoginServlet extends HttpServlet {
 
 	private static final Logger log = LoggerFactory.getLogger(LoginServlet.class);
+	private static final int HOUR = 3600;
+	private static final String HOME = "/";
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
@@ -39,6 +42,15 @@ public class LoginServlet extends HttpServlet {
 		}
 
 		log.debug("LOGIN COMPLETE !");
-		// TODO : Cookie 발급
+		resp.addCookie(createCookie("login", "true"));
+		resp.sendRedirect(HOME);
 	}
+
+	private Cookie createCookie(String name, String value) {
+		Cookie cookie = new Cookie(name, value);
+		cookie.setMaxAge(HOUR);
+		cookie.setPath(HOME);
+		return cookie;
+	}
+
 }
